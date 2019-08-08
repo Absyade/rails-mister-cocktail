98ca3490145cc7210854decaa13ccc17ae8d46a9
Abs Yade <absyade@gmail.com>
2019-08-08 15:13:38 +0200

    generated doses controller

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,26 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(strong_params_cocktail)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def strong_params_cocktail
+    params.require(:cocktail).permit(:name)
+  end
+end
